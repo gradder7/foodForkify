3d0db78cd7b3d8d7d73b4ea3513057ab4094d9fd
Himanshu <mehra3228@gmail.com>
2023-03-25 18:14:26 +0530

    MVC artitechture

--- src/js/model.js
+++ src/js/model.js
@@ -1,9 +1,35 @@
-import { async } from "regenerator-runtime"
+import { async } from 'regenerator-runtime';
 
-export const state ={
-    recipe:{},
-}
+// for hadelling all the data
+export const state = {
+  recipe: {},
+};
 
-export const loadRecipe= async()=>{
-    
-}
+// This function will change the state object
+export const loadRecipe = async function (id) {
+  try {
+    const res = await fetch(
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
+    );
+    const data = await res.json();
+    if (!res.ok) throw new Error(`${data.message} ${res.status}`);
+    console.log(res, data);
+    //destructure
+    const { recipe } = data.data;
+
+    //define the data to own key
+    state.recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+    console.log(state.recipe);
+  } catch (err) {
+    alert(err);
+  }
+};
